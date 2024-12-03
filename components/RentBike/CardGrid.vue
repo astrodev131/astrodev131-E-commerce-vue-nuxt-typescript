21a27812bc0156fc12329a7c1d72d4053d309a9b
astrodev131 <astro.dev131@gmail.com>
2024-12-03 23:56:03 +0900

    first-complete in cardgrid

---
+++ components/RentBike/CardGrid.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="flex w-10/12 justify-center mx-auto gap-10 flex-wrap my-5">
+    <Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card />
+  </section>
+</template>
+<script lang="ts" setup>
+let arr: Array<undefined> = new Array(5);
+import Card from "./Card.vue";
+</script>
