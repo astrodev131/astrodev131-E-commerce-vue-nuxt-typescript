7034f7ea0bccdc1c9339de471424a45e71a0fc0f
astrodev131 <astro.dev131@gmail.com>
2024-12-04 17:30:09 +0900

    complete the rentbike

--- components/RentBike/HeroSection.vue
+++ components/RentBike/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="grid grid-cols-7 gap-4"
+    class="grid grid-cols-1 md:grid-cols-7 gap-4"
     style="
       background-image: url('images/bg2.png');
       background-size: cover;
@@ -8,14 +8,21 @@
       background-repeat: no-repeat;
     "
   >
-    <div class="grid grid-cols-subgrid mt-[7%] gap-4 col-span-5">
-      <div class="col-start-3 text-center col-span-4 text-white text-5xl">
+    <div class="grid grid-cols-1 md:grid-cols-subgrid mt-[7%] gap-4 col-span-5">
+      <div
+        class="col-start-1 md:col-start-3 text-center col-span-1 md:col-span-4 text-white text-3xl md:text-5xl"
+      >
         Rent A Bike Rent Your Freedom
       </div>
     </div>
 
-    <div class="w-80 col-start-6 h-80">
-      <img class="w-80 h-80" src="/public/images/bycycle.svg" />
+    <div
+      class="w-60 md:w-80 col-start-1 md:col-start-6 h-60 md:h-80 mx-auto md:mx-0"
+    >
+      <img
+        class="w-full h-full object-contain"
+        src="/public/images/bycycle.svg"
+      />
     </div>
   </section>
 </template>
