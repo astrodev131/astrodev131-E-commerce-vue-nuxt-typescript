4f39fa1a54245fbc716a0ca580cd34dd34cffc4b
astrodev131 <astro.dev131@gmail.com>
2024-12-20 13:50:49 -0600

    finish dashboard basically

---
+++ components/Dashboard/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Hero></Hero>
+  <div class="w-10/12 m-auto"><Features></Features></div>
+</template>
+<script lang="ts" setup>
+import Hero from "./Hero.vue";
+import Features from "./Features.vue";
+</script>
